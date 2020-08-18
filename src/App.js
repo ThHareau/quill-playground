542f363bc900eb19eacc519fde2b92c2df795ac9
Thomas Hareau <thomas.hareau@doctolib.com>
2020-08-18 16:05:52 +0200

    Eslint

--- src/App.js
+++ src/App.js
@@ -9,16 +9,13 @@ const App = () => {
         {
             name: 'last-name',
             value: '',
-            displayName: '',
         },
         {
             name: 'first-name',
             value: '',
-            displayName: '',
         }
     ])
     const ref = useRef()
-    const [clickedVariable, setClickedVariable] = useState()
     const [template, setTemplate] = useState('')
     const [text, setText] = useState(template)
 
@@ -29,17 +26,19 @@ const App = () => {
         <div style={{display: 'flex'}}>
             <div style={{border: 1, flex: 4}}>
                 <h3>Quill</h3>
-                <p>Clicked variable: {clickedVariable?.value || ''}</p>
                 <TemplateEditor text={template} onChange={setTemplate} ref={ref}/>
             </div>
             <div style={{flex: 1, display: 'flex', flexDirection: 'column'}}>
                 <h3>Variables</h3>
                 {variables.filter(({value}) => !!value)
-                    .map((variable) => <button key={variable.name}
-                                               onClick={() => {
-                                                   setClickedVariable(variable)
-                                                   ref.current.variable(variable)
-                                               }}>{variable.name}</button>)
+                    .map((variable) =>
+                        <button
+                            key={variable.name}
+                            onClick={() => ref.current.variable(variable)}
+                        >
+                            {variable.name}
+                        </button>
+                    )
                 }
             </div>
         </div>
