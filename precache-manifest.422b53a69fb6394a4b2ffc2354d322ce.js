d3b71eb3178b82ffcace979db4583c1216648002
Thomas Hareau <thomas.hareau@doctolib.com>
2020-09-15 14:32:41 +0200

    Updates

---
+++ precache-manifest.422b53a69fb6394a4b2ffc2354d322ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12314f612c048097b147c6b575a88dc",
+    "url": "/quill-playground/index.html"
+  },
+  {
+    "revision": "b5ad078e01ebeeb76cd5",
+    "url": "/quill-playground/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "7fd1a1be7f1249643bc1",
+    "url": "/quill-playground/static/css/main.d6d0f4e8.chunk.css"
+  },
+  {
+    "revision": "b5ad078e01ebeeb76cd5",
+    "url": "/quill-playground/static/js/2.e7abf502.chunk.js"
+  },
+  {
+    "revision": "9598fde2a22ec3ce6af9cd5c173a9b46",
+    "url": "/quill-playground/static/js/2.e7abf502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd1a1be7f1249643bc1",
+    "url": "/quill-playground/static/js/main.107c08c5.chunk.js"
+  },
+  {
+    "revision": "a34bdc6b2333531ec554",
+    "url": "/quill-playground/static/js/runtime-main.99fa209b.js"
+  }
+]);
